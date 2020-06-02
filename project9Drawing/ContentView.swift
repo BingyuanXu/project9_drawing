836a143158a17fda52437596f10b7a68e0cd38e9
BingyuanXu <xubingyuan123@hotmail.com>
2020-06-02 08:01:04 -0500

    Initial Commit

---
+++ project9Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project9Drawing
+//
+//  Created by bingyuan xu on 2020-06-02.
+//  Copyright © 2020 bingyuan xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
