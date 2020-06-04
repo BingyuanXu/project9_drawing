15e02ff35900fabaad8c9b1d7ccc370df22c1d0a
BingyuanXu <xubingyuan123@hotmail.com>
2020-06-03 20:53:00 -0500

    using path to draw

--- project9Drawing/ContentView.swift
+++ project9Drawing/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      VStack {
         Text("Hello, World!")
+        Path { path in
+          path.move(to: CGPoint(x: 200, y: 100))
+          path.addLine(to: CGPoint(x: 100, y: 300))
+          path.addLine(to: CGPoint(x: 300, y: 300))
+          path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+//        .fill(Color.blue)
+          .stroke(Color.blue.opacity(0.5),
+                  style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+        )
+        
+      }
     }
 }
 
